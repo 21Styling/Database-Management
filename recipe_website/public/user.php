30586c2979d628875338b6952e58ae60b0900da2
Peter Platzer <26platzer@gmail.com>
2025-05-06 17:07:38 -0500

    Update user.php

--- recipe_website/public/user.php
+++ recipe_website/public/user.php
@@ -11,9 +11,12 @@ if (!isset($_SESSION['username'])) {
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="style.css">
 <html>
+<header class="site-header">
+<h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
+<p><a href="index.php">&laquo; Back to Home</a></p>
+    </header>
 <head><title>Your Account</title></head>
 <body>
-  <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
   <ul>
     <li><a href="ingredients.php">Enter Owned Ingredients</a></li>
     <li><a href="favorites.php">View Favorite Recipes</a></li>
