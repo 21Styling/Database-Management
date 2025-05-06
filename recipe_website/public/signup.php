2cf8815ea5df67e7bb2967012ca160bc1197aca9
Peter Platzer <26platzer@gmail.com>
2025-05-06 16:22:10 -0500

    Update signup.php

--- recipe_website/public/signup.php
+++ recipe_website/public/signup.php
@@ -6,6 +6,10 @@ require_once __DIR__ . '/../src/db_connect.php'; ?>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="style.css">
   <title>Sign up</title>
 </head>
 <body>
