f3d351e11d9f17a20ebec8f0bad37e084663629e
Peter Platzer <26platzer@gmail.com>
2025-05-08 13:23:09 -0500

    Delete recipe_website directory

--- recipe_website/config/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'washington.uww.edu');       // Your specific hostname
-define('DB_NAME', 'cs366-2251_mitchellcd24');  // Your specific database name
-define('DB_USER', 'mitchellcd24');           // Your specific username (netID)
-define('DB_PASS', 'cm9724');                 // Your specific password
-define('DB_CHARSET', 'utf8mb4');               // Recommended charset
-
-?>
